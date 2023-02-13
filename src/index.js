ec94f0cfe661e33f0f3b45a9a407f977a05e867b
ajiltOk <21summer12@gmail.com>
2023-02-13 21:32:01 +0300

    feat: add the solution of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let newN = n;  
+  if(n < 0) {
+    newN = n * (-1);
+  } else if(n % 10 == 0) {
+    newN = n / 10;
+  }
+  let arr = (String(newN)).split('');
+  return arr.reverse().join('');
 }
